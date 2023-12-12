4cb9978becf46ea44ae7c946eb619432be474606
Evan <egoldsbo@gmail.com>
2023-12-11 23:41:31 -0800

    newscript

--- script.py
+++ script.py
@@ -1,49 +1,35 @@
-""" sudo apt-get install python3-pygame """
-
-import pygame
+import vlc
 import sys
-from pygame.locals import *
-import barcode
 
-# Initialize Pygame
-pygame.init()
-
-# Set the display to fullscreen
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+# Dictionary mapping barcodes to video file paths
+barcode_to_video = {
+    "walk": "./vids/walk.mp4"
+ 
+    # Add more barcodes and corresponding video paths here
+}
 
 def play_video(video_path):
-    """ Play the video at the given path fullscreen. """
-    # Load the video
-    movie = pygame.movie.Movie(video_path)
+    """ Play video at the given path """
+    player = vlc.MediaPlayer(video_path)
+    player.set_fullscreen(True)
+    player.play()
+    while player.is_playing():
+        pass
+    player.stop()
 
-    # Get the size of the movie
-    movie_screen = pygame.Surface(movie.get_size()).convert()
+def main():
+    try:
+        while True:
+            barcode = input("Scan barcode: ")
+            video_path = barcode_to_video.get(barcode)
+            if video_path:
+                print(f"Playing video for barcode {barcode}")
+                play_video(video_path)
+            else:
+                print("No video found for this barcode.")
+    except KeyboardInterrupt:
+        print("Program exited.")
+        sys.exit()
 
-    # Set the movie screen as the target for the movie
-    movie.set_display(movie_screen)
-
-    # Start playing the movie
-    movie.play()
-
-    # Keep playing until the movie is finished or quit is detected
-    while movie.get_busy():
-        screen.blit(movie_screen,(0,0))
-        pygame.display.update()
-
-        # Check for quit events
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                movie.stop()
-                pygame.quit()
-                sys.exit()
-
-# Barcode scanner input setup
-scanner = barcode.get_scanner('usb') # Adjust as needed for your scanner
-
-while True:
-    barcode_data = scanner.get_barcode() # Get barcode data
-
-    if barcode_data:
-        # Assuming barcode data corresponds to the video filename
-        video_path = f'/vids/{barcode_data}.mp4'
-        play_video(video_path)
+if __name__ == "__main__":
+    main()
