c3b75605a3d7cf01ef29dbbfba61d5d3652757fb
Evan <egoldsbo@gmail.com>
2023-12-14 22:17:02 -0800

    tkinter

--- script.py
+++ script.py
@@ -1,15 +1,10 @@
+import tkinter as tk
 import vlc
 import time
 
-def play_video(video_path):
-    # Create a VLC instance
-    player = vlc.Instance()
-
-    # Create a new player
-    media_player = player.media_player_new()
-
+def play_video(video_path, media_player):
     # Load the media file
-    media = player.media_new(video_path)
+    media = vlc.Media(video_path)
 
     # Set the media player media
     media_player.set_media(media)
@@ -17,25 +12,37 @@ def play_video(video_path):
     # Play the video
     media_player.play()
 
-    time.sleep(3)
-
-    # Set to full screen
-    media_player.set_fullscreen(True)
-
     # Wait for the video to finish
     while media_player.get_state() != vlc.State.Ended:
         time.sleep(1)
 
-while True:
-    # Ask the user to input the filename
-    filename = input("filename (or type 'exit' to quit): ")
-
-    # Check for 'exit' condition
-    if filename.lower() == 'exit':
-        break
-
-    # Append the path and file extension
+def start_video():
+    filename = entry.get()
     video_path = "./vids/" + filename + ".mp4"
-    
-    # Play the video
-    play_video(video_path)
+    play_video(video_path, media_player)
+
+# Create a Tkinter window
+root = tk.Tk()
+root.title("VLC Media Player")
+root.geometry("800x600")
+
+# Create a VLC player
+player = vlc.Instance()
+media_player = player.media_player_new()
+
+# Create a Frame to embed the VLC player
+video_frame = tk.Frame(root, bg="black")
+video_frame.pack(fill=tk.BOTH, expand=True)
+
+# Set the VLC player's drawable to the frame's window ID
+media_player.set_xwindow(video_frame.winfo_id())
+
+# Entry widget for video filename
+entry = tk.Entry(root)
+entry.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=10)
+
+# Play button
+play_button = tk.Button(root, text="Play Video", command=start_video)
+play_button.pack(side=tk.BOTTOM)
+
+root.mainloop()
