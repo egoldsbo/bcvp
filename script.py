096bdece655f2de7c90b5b8f23d5e31344f74f83
Evan <egoldsbo@gmail.com>
2023-12-15 21:36:49 -0800

    prompt again

--- script.py
+++ script.py
@@ -1,21 +1,29 @@
 import subprocess
 import os
 
-# Directory where the video files are stored
-video_directory = './vids/'
-
-# Prompt the user to enter the name of the video file
-video_file_name = input("Enter the name of the video file to play: ")
-
-# Full path to the video file
-video_file_path = os.path.join(video_directory, video_file_name)+'.mp4'
-
-# Check if the file exists
-if not os.path.exists(video_file_path):
-    print("Video file not found.")
-else:
+def play_video(video_path):
     # Command to play video using FFmpeg
-    play_command = ['ffplay','-fs', video_file_path]
+    play_command = ['ffplay', '-fs', video_path]
 
     # Execute the command
     subprocess.run(play_command)
+
+# Directory where the video files are stored
+video_directory = './vids/'
+
+while True:
+    # Prompt the user to enter the name of the video file
+    video_file_name = input("Enter the name of the video file to play or 'exit' to quit: ")
+
+    # Check if the user wants to exit the program
+    if video_file_name.lower() == 'exit':
+        break
+
+    # Full path to the video file
+    video_file_path = os.path.join(video_directory, video_file_name) + '.mp4'
+
+    # Check if the file exists
+    if not os.path.exists(video_file_path):
+        print("Video file not found. Please try again.")
+    else:
+        play_video(video_file_path)
