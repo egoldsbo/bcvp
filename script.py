4c00d8596d6a047474044ecbd5cfe4ab7b205528
Evan <egoldsbo@gmail.com>
2023-12-15 22:20:04 -0800

    window size and orgin

--- script.py
+++ script.py
@@ -1,12 +1,27 @@
 import subprocess
 import os
 
-def play_video(video_path):
+def play_video(video_path, width, height, x_position, y_position):
     # Command to play video using FFmpeg
-    # The '-autoexit' flag is added to ensure ffplay closes after the video ends
-    play_command = ['ffplay', '-x', str(800), '-y', str(480), '-autoexit', video_path]
+    play_command = [
+        'ffplay', 
+        '-x', str(width), 
+        '-y', str(height), 
+        '-autoexit', 
+        '-left', str(x_position), 
+        '-top', str(y_position), 
+        video_path
+    ]
+
     # Execute the command and wait for it to finish
     subprocess.run(play_command)
+
+video_width = 800  # Example width
+video_height = 480 # Example height
+
+# Desired window position
+x_position = 0  # Example X coordinate
+y_position = 0  # Example Y coordinate
 
 # Directory where the video files are stored
 video_directory = './vids/'
@@ -26,7 +41,7 @@ while True:
     if not os.path.exists(video_file_path):
         print("Video file not found. Please try again.")
     else:
-        play_video(video_file_path)
+        play_video(video_file_path, video_width, video_height, x_position, y_position)
 
     # Refocus to the terminal (optional)
     # subprocess.run(['wmctrl', '-a', 'Terminal']) # Uncomment if window manager supports it
