49c519d04a4280f38cd043ece2a5e9ade755b105
Evan <egoldsbo@gmail.com>
2023-12-17 01:04:03 -0800

    onoff 2

--- onoff.py
+++ onoff.py
@@ -2,11 +2,10 @@ import subprocess
 import time
 
 def turn_off_hdmi():
-    subprocess.call('tvservice -o', shell=True)
+    subprocess.call('vcgencmd display_power 0', shell=True)
 
 def turn_on_hdmi():
-    subprocess.call('tvservice -p', shell=True)
-    subprocess.call('fbset -depth 8; fbset -depth 16', shell=True)
+    subprocess.call('vcgencmd display_power 1', shell=True)
 
 # Example usage
 turn_off_hdmi()
