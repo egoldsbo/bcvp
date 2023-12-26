9da63202015031eaff0cd5925425d0f66b653d90
Evan <egoldsbo@gmail.com>
2023-12-25 22:48:03 -0800

    open and run

--- openterminal.py
+++ openterminal.py
@@ -1,15 +1,20 @@
 import subprocess
 
-def open_terminal():
-    # Command to open a new terminal window
-    terminal_command = "lxterminal"
+def open_terminal_and_run_commands():
+    # Directory and script you want to run
+    directory = "bcvp"
+    script = "script.py"
 
-    # Open a new terminal window
+    # Command to open a new terminal window and run the commands
+    # Using bash -c allows for running multiple commands in sequence
+    terminal_command = ["lxterminal", "-e", f"bash -c 'cd {directory}; python3 {script}; exec bash'"]
+
+    # Open a new terminal window and run the commands
     try:
         subprocess.Popen(terminal_command)
-        print("Terminal opened successfully.")
+        print("Terminal opened and commands executed successfully.")
     except Exception as e:
-        print(f"Error opening terminal: {e}")
+        print(f"Error opening terminal or executing commands: {e}")
 
 if __name__ == "__main__":
-    open_terminal()
+    open_terminal_and_run_commands()
