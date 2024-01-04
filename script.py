b502e60bbee0c1b66a71267b5fb55323100e5b62
Evan <egoldsbo@gmail.com>
2024-01-03 23:25:47 -0800

    vlc test

--- script.py
+++ script.py
@@ -3,44 +3,29 @@ import os
 import threading
 import time
 
-# Directory where the video files are stored
-video_directory = '/home/pi/bcvp/vids/'
-
-# Preload videos into memory
-videos_in_memory = {}
-for video_file_name in os.listdir(video_directory):
-    video_file_path = os.path.join(video_directory, video_file_name)
-    if os.path.isfile(video_file_path):
-        with open(video_file_path, 'rb') as video_file:
-            videos_in_memory[video_file_name] = video_file.read()
-
-def play_video_from_memory(video_data, video_name):
-    # Path for the temporary video file
-    temp_video_path = '/dev/shm/' + video_name
-
-    # Write video data to a temporary file in RAM
-    with open(temp_video_path, 'wb') as temp_video_file:
-        temp_video_file.write(video_data)
-
-    # Command to play video using VLC
-    play_command = ['cvlc', '--play-and-exit', '--no-audio', temp_video_path]
+def play_video(video_path):
+    # Command to play video using FFmpeg in fullscreen mode
+    play_command = ['cvlc','--no-audio','--play-and-exit', video_path]
 
     # Execute the command and wait for it to finish
     subprocess.run(play_command)
 
-    # Remove the temporary file
-    os.remove(temp_video_path)
+# Directory where the video files are stored
+video_directory = '/home/pi/bcvp/vids/'
 
 while True:
     # Prompt the user to enter the name of the video file
-    video_file_name = input("Enter the name of the video file to play or 'exit' to quit: ") + '.mp4'
+    video_file_name = input("Enter the name of the video file to play or 'exit' to quit: ")
 
     # Check if the user wants to exit the program
-    if video_file_name.lower() == 'exit.mp4':
+    if video_file_name.lower() == 'exit':
         break
 
-    # Check if the file is preloaded in memory
-    if video_file_name in videos_in_memory:
-        play_video_from_memory(videos_in_memory[video_file_name], video_file_name)
-    else:
+    # Full path to the video file
+    video_file_path = os.path.join(video_directory, video_file_name) + '.mp4'
+
+    # Check if the file exists
+    if not os.path.exists(video_file_path):
         print("Video file not found. Please try again.")
+    else:
+        play_video(video_file_path)
