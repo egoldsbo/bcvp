a7fe8476139b2594b6a63676ef71c61cdc4b15f8
Evan <egoldsbo@gmail.com>
2024-01-03 23:12:38 -0800

    exp2

--- script.py
+++ script.py
@@ -3,48 +3,44 @@ import os
 import threading
 import time
 
-# Preload a small part of each video
-def preload_videos(video_directory, preload_size=1024*1024):  # Preload the first 1MB of each video
-    preloaded_videos = {}
-    for filename in os.listdir(video_directory):
-        if filename.endswith(".mp4"):
-            file_path = os.path.join(video_directory, filename)
-            with open(file_path, 'rb') as file:
-                preloaded_videos[filename] = file.read(preload_size)
-    return preloaded_videos
+# Directory where the video files are stored
+video_directory = '/home/pi/bcvp/vids/'
 
-def play_video(video_path, preloaded_data=None):
-    # Write preloaded data to a temporary file
-    with open('/tmp/temp_video.mp4', 'wb') as temp_file:
-        temp_file.write(preloaded_data)
+# Preload videos into memory
+videos_in_memory = {}
+for video_file_name in os.listdir(video_directory):
+    video_file_path = os.path.join(video_directory, video_file_name)
+    if os.path.isfile(video_file_path):
+        with open(video_file_path, 'rb') as video_file:
+            videos_in_memory[video_file_name] = video_file.read()
 
-    # Command to play video using mpv
-    play_command = ['mpv', '--no-resume-playback', '/tmp/temp_video.mp4', video_path]
+def play_video_from_memory(video_data, video_name):
+    # Path for the temporary video file
+    temp_video_path = '/dev/shm/' + video_name
+
+    # Write video data to a temporary file in RAM
+    with open(temp_video_path, 'wb') as temp_video_file:
+        temp_video_file.write(video_data)
+
+    # Command to play video using FFmpeg in fullscreen mode
+    play_command = ['mpv', temp_video_path]
 
     # Execute the command and wait for it to finish
     subprocess.run(play_command)
 
-# Directory where the video files are stored
-video_directory = '/home/pi/bcvp/vids/'
-
-# Preload videos
-preloaded_videos = preload_videos(video_directory)
+    # Remove the temporary file
+    os.remove(temp_video_path)
 
 while True:
-    video_file_name = input("Enter the name of the video file to play or 'exit' to quit: ")
-    if video_file_name.lower() == 'exit':
+    # Prompt the user to enter the name of the video file
+    video_file_name = input("Enter the name of the video file to play or 'exit' to quit: ") + '.mp4'
+
+    # Check if the user wants to exit the program
+    if video_file_name.lower() == 'exit.mp4':
         break
 
-    video_file_path = os.path.join(video_directory, video_file_name) + '.mp4'
-
-    if not os.path.exists(video_file_path):
-        print("Video file not found. Please try again.")
+    # Check if the file is preloaded in memory
+    if video_file_name in videos_in_memory:
+        play_video_from_memory(videos_in_memory[video_file_name], video_file_name)
     else:
-        # Retrieve preloaded data if available
-        preloaded_data = preloaded_videos.get(video_file_name + '.mp4', None)
-
-        # Play the video using preloaded data
-        if preloaded_data:
-            play_video(video_file_path, preloaded_data)
-        else:
-            play_video(video_file_path)
+        print("Video file not found. Please try again.")
