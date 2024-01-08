0644b82c87d92407b7712f85803034c580c5ed19
Evan <egoldsbo@gmail.com>
2024-01-07 22:02:51 -0800

    added video debounce

--- script.py
+++ script.py
@@ -1,11 +1,9 @@
 import subprocess
 import os
 
-subprocess.run(['git', 'pull'])
-
 def play_video(video_path):
-    # Command to play video using FFmpeg in fullscreen mode
-    play_command = ['cvlc', '--no-audio', '--play-and-exit', video_path]
+    # Command to play video using VLC in fullscreen mode
+    play_command = ['cvlc', '--fullscreen', '--play-and-exit', video_path]
 
     # Execute the command and wait for it to finish
     subprocess.run(play_command)
@@ -15,24 +13,22 @@ video_directory = '/home/pi/bcvp/vids/'
 
 while True:
     # Prompt the user to enter the name of the video file
-    video_file_name = input("Enter the name of the video file to play, 'gitupdate' to update, or 'exit' to quit: ")
+    video_file_name = input("Enter the name of the video file to play, 'git' to update, or 'exit' to quit: ")
 
     # Check if the user wants to exit the program
     if video_file_name.lower() == 'exit':
         break
 
     # Check if the user wants to run 'git pull'
-    if video_file_name.lower() == 'git':
+    elif video_file_name.lower() == 'git':
         # Run 'git pull' command
         subprocess.run(['git', 'pull'])
-        # Exit the loop
-        break
-
-    # Full path to the video file
-    video_file_path = os.path.join(video_directory, video_file_name) + '.mp4'
-
-    # Check if the file exists
-    if not os.path.exists(video_file_path):
-        print("Video file not found. Please try again.")
     else:
-        play_video(video_file_path)
+        # Full path to the video file
+        video_file_path = os.path.join(video_directory, video_file_name) + '.mp4'
+
+        # Check if the file exists
+        if not os.path.exists(video_file_path):
+            print("Video file not found. Please try again.")
+        else:
+            play_video(video_file_path)
