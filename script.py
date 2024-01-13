5f46cbc13c1b8146a534b098a55de75f8f14bfb2
Evan <egoldsbo@gmail.com>
2024-01-12 19:58:23 -0800

    update

--- script.py
+++ script.py
@@ -1,57 +1,53 @@
 import subprocess
 import os
 
-def play_video(video_path):
-    # Command to play video using VLC in fullscreen mode
+def play_video(video_path, single_play=False):
     play_command = ['vlc',  # Using cvlc (command-line VLC)
-                    '--no-osd', 
-                    '--no-audio', 
-                    '--fullscreen', 
+                    '--no-osd',
+                    '--no-audio',
+                    '--fullscreen',
                     #'--avcodec-hw=none',  # Disable hardware acceleration
                     '--file-caching=100',  # Adjust file caching
                     '--no-loop',
                     '--play-and-exit',  # Exit VLC after playing the video
                     '/home/pi/bcvp/vids/blackscreen.mp4',  # Path to the video file
-                    video_path,
-                    video_path,
-                    '>/dev/null 2>&1']
+                    video_path]
+
+    # Add video_path again if not in single play mode
+    if not single_play:
+        play_command.append(video_path)
+
+    play_command.append('>/dev/null 2>&1')
+
     # Execute the command and wait for it to finish
     subprocess.run(play_command)
-
-    # Wait for the video to end and then pause it
- 
 
 
 # Directory where the video files are stored
 video_directory = '/home/pi/bcvp/vids/'
 
+# Array containing video file names to play only once
+single_play_videos = ['dinor.mp4','ddd.mp4']# Replace with actual video names
+
 # Flag to indicate if a video is playing
 video_playing = False
 
 while True:
-    # Check if a video is not playing
     if not video_playing:
-        # Prompt the user to enter the name of the video file
         video_file_name = input("Enter the name of the video file to play, 'git' to update, or 'exit' to quit: ")
 
-        # Check if the user wants to exit the program
         if video_file_name.lower() == 'exit':
             break
-
-        # Check if the user wants to run 'git pull'
         elif video_file_name.lower() == 'git':
             subprocess.run(['sudo', './gitscript.sh'])
-           
         else:
-            # Full path to the video file
             video_file_path = os.path.join(video_directory, video_file_name) + '.mp4'
 
-            # Check if the file exists
             if not os.path.exists(video_file_path):
                 print("Video file not found. Please try again.")
             else:
-                # Set the flag to True, indicating a video is playing
                 video_playing = True
-                play_video(video_file_path)
-                # Reset the flag to False after the video is done
+                # Check if the video is in the single play list
+                single_play = video_file_name in single_play_videos
+                play_video(video_file_path, single_play)
                 video_playing = False
